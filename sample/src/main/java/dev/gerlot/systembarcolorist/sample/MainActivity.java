a9b2d98b64b8fd8e9d00c54a02aea778b387aec4
Balázs Gerlei <gerlotdev@gmail.com>
2023-05-18 11:57:02 +0200

    Start adding functionality to change top and bottom colors for the gradient background to the sample app
    - Add buttons that display the current selected colors: one for the top color and one for the bottom
    - Add example gradient and colors so that in the design view the default gradient is shown

--- sample/src/main/java/dev/gerlot/systembarcolorist/sample/MainActivity.java
+++ sample/src/main/java/dev/gerlot/systembarcolorist/sample/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,8 +19,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.main_activity);
         final View backgroundView = ((ViewGroup) this.findViewById(android.R.id.content)).getChildAt(0);
         if (backgroundView != null) {
-            final int topColorInt = Color.LTGRAY;
-            final int bottomColorInt = Color.BLUE;
+            final String topColorString = "#FFCCCCCC";
+            final int topColorInt = Color.parseColor(topColorString);
+            final Button topColorButton = findViewById(R.id.topColorButton);
+            topColorButton.setText(topColorString);
+            final String bottomColorString = "#FF0000FF";
+            final int bottomColorInt = Color.parseColor(bottomColorString);
+            final Button bottomColorButton = findViewById(R.id.bottomColorButton);
+            bottomColorButton.setText(bottomColorString);
             final GradientDrawable gd = new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, new int[] { topColorInt, bottomColorInt });
             gd.setCornerRadius(0f);
             backgroundView.setBackground(gd);
